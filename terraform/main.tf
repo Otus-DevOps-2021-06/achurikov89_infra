14a84b2fcd4b3e6e207dbbd2af3754e39ea48d4d
achurikov89 <achurikov@express42.com>
2021-06-20 19:57:15 +0000

    mod terraform code

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,8 @@
 provider "yandex" {
   service_account_key_file = var.service_account_key_file
-  cloud_id  = var.cloud_id
-  folder_id = var.folder_id
-  zone      = var.zone
+  cloud_id                 = var.cloud_id
+  folder_id                = var.folder_id
+  zone                     = var.zone
 }
 
 resource "yandex_compute_instance" "app" {
@@ -13,7 +13,7 @@ resource "yandex_compute_instance" "app" {
     memory = 2
   }
 
-boot_disk {
+  boot_disk {
     initialize_params {
       # Указать id образа созданного в предыдущем домашем задании
       image_id = var.image_id
@@ -27,25 +27,25 @@ boot_disk {
   }
 
   metadata = {
-  ssh-keys = "ubuntu:${file(var.public_key_path)}"
+    ssh-keys = "ubuntu:${file(var.public_key_path)}"
   }
 
-connection {
-  type = "ssh"
-  host = yandex_compute_instance.app.network_interface.0.nat_ip_address
-  user = "ubuntu"
-  agent = false
-  # путь до приватного ключа
-  private_key = file("~/.ssh/ubuntu")
+  connection {
+    type  = "ssh"
+    host  = yandex_compute_instance.app.network_interface.0.nat_ip_address
+    user  = "ubuntu"
+    agent = false
+    # путь до приватного ключа
+    private_key = file(var.private_key_path)
   }
 
-provisioner "file" {
-source = "files/puma.service"
-destination = "/tmp/puma.service"
-}
+  provisioner "file" {
+    source      = "files/puma.service"
+    destination = "/tmp/puma.service"
+  }
 
-provisioner "remote-exec" {
-  script = "files/deploy.sh"
-}
+  provisioner "remote-exec" {
+    script = "files/deploy.sh"
+  }
 }
 
