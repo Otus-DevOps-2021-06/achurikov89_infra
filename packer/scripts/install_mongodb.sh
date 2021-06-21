979a19de0e61c7c4086c38d6e932430861b520fc
achurikov89 <achurikov@express42.com>
2021-06-21 16:08:15 +0000

    Create Modules terraform-2

--- packer/scripts/install_mongodb.sh
+++ packer/scripts/install_mongodb.sh
@@ -2,6 +2,9 @@
 wget -qO - https://www.mongodb.org/static/pgp/server-3.2.asc | sudo apt-key add -
 echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 apt-get update
+sudo rm /var/lib/apt/lists/lock
+sudo rm /var/cache/apt/archives/lock
+sudo rm /var/lib/dpkg/lock*
 apt install -y mongodb-org
 systemctl start mongod
 systemctl enable mongod
